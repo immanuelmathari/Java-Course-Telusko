d4900844a538ec1d1536f6e206cceadcde965d29
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-16 23:23:19 +0300

    Lesson 12: Type Conversions and casting

--- Hello01.java
+++ Hello01.java
@@ -1,6 +1,33 @@
 // 16.08.25
 public class Hello01 {
     public static void main(String[] args) {
-        System.out.println("Hello world from Java. I am 23");
+        int num = 23;
+        int num1 = 23;
+        int num2 = 25;
+        int result = num1 + num2;
+        float marks = 6.5f;
+        byte bt = 127; // is the last value we can store in a byte
+        short sh = 558;
+        long l = 5845l;
+        float f = 5.8f;
+        char manuinitial = 'm';
+        String name = "Ales";
+        // literals
+        // base 2
+        int num3 = 0b101; 
+        // hexadecimal numbers
+        int num4 = 0x7E;
+        // separating numbers
+        int num5 = 10_00_00_00;
+        double num6 = 56;
+        double num7 = 12e10;
+        char num8 = 'a';
+        num8++;
+        byte b = 127;
+        System.out.println("Hello world from Java. I am " + num);
+        System.out.println("The sum of my age and Kesh is "+ result);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num8);
     }
 }
