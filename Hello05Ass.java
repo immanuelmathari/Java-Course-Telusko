5ab9279c6427102d16860d8d08df915426443416
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-19 12:17:30 +0300

    Coding Exercises 2: Mastering Java Operators: Arithmetic, Relational, and Logical in Action

---
+++ Hello05Ass.java
@@ -0,0 +1,24 @@
+public class Hello05Ass {
+    public static void main(String[] args){
+     
+         // write code here
+         int a = 20;
+         int b = 10;
+         
+         int result = a + b;
+         int result2 = a - b;
+         int result3 = a * b;
+         int result4 = a / b;
+         
+         boolean result5 = a > b;
+         boolean result6 = (a>b) && (b>0);
+         
+         System.out.println("Addition: " +result);
+         System.out.println("Subtraction: " +result2);
+         System.out.println("Multiplication: " +result3);
+         System.out.println("Division: " +result4);
+         System.out.println("Is a greater than b? " +result5);
+         System.out.println("Is a > b and b > 0? " +result6);
+         
+    }
+}
