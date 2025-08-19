ce38cb6c5fe47a089a2c05283050c4057b6cd077
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-08-19 11:57:45 +0300

    Lesson 14: Relational Operators

---
+++ Hello03.java
@@ -0,0 +1,21 @@
+public class Hello03 {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        int z = 6;
+
+        double a = 8.8;
+        double b = 9.8;
+
+        boolean result = (x < y);
+        boolean result2 = (x > y);
+        boolean result3 = (z != y);
+        boolean result4 = (x == z);
+        // the brakets aint a must lil kid
+
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
